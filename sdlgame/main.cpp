c8aa25957ce9848c7230d3b35dfcb19aa7193f3d
Paul Cohn <pscohn@gmail.com>
2017-11-20 22:23:21 -0500

    more sdl stuff

--- sdlgame/main.cpp
+++ sdlgame/main.cpp
@@ -3,13 +3,25 @@
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+enum KeyPressSurfaces {
+  KEY_PRESS_SURFACE_DEFAULT,
+  KEY_PRESS_SURFACE_UP,
+  KEY_PRESS_SURFACE_DOWN,
+  KEY_PRESS_SURFACE_LEFT,
+  KEY_PRESS_SURFACE_RIGHT,
+  KEY_PRESS_SURFACE_TOTAL,
+}
+
 
 bool init();
 bool loadMedia();
 void close();
+SDL_Surface* loadSurface(std::string path);
 
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+SDL_Surface* gCurrentSurface = NULL;
 SDL_Surface* gHelloWorld = NULL; // image
 
 bool init() {
@@ -18,7 +30,7 @@ bool init() {
     printf("SDL could not intialize. SDL_Error: %s\n", SDL_GetError());
     success = false;
   } else {
-    //Create window
+    // Create window
     gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (gWindow == NULL) {
       printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -31,11 +43,19 @@ bool init() {
   return success;
 }
 
+SDL_Surface* loadSurface(std::string path) {
+  SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+  if (loadedSurface == NULL) {
+    printf("Unable to load image %s. SDL Error: %s\n", path.c_str(), SDL_GetError());
+  }
+  return loadedSurface;
+}
+
 bool loadMedia() {
   bool success = true;
-  gHelloWorld = SDL_LoadBMP("hello_world.bmp");
+  gHelloWorld = loadSurface("images/hello_world.bmp");
   if (gHelloWorld == NULL) {
-    printf("Unable to load image %s! SDL Error: %s\n", "hello_world.bmp", SDL_GetError());
+    printf("Unable to load image %s! SDL Error: %s\n", "images/hello_world.bmp", SDL_GetError());
     success = false;
   }
   return success;
