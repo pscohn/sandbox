988ed616e68cf2fb042a6d48385bc10d8d76a9c3
Paul Cohn <pscohn@gmail.com>
2017-12-03 20:43:21 -0500

    refactors and detect click

--- sdlgame/src/main.cpp
+++ sdlgame/src/main.cpp
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <string>
 #include "player.h"
+#include "window.h"
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
 enum KeyPressSurfaces {
   KEY_PRESS_SURFACE_DEFAULT,
   KEY_PRESS_SURFACE_UP,
@@ -16,72 +15,17 @@ enum KeyPressSurfaces {
   KEY_PRESS_SURFACE_TOTAL,
 };
 
-bool init();
 bool loadMedia();
 void close();
 SDL_Surface* loadSurface(std::string path);
 
-SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
 SDL_Surface* gCurrentSurface = NULL;
-SDL_Surface* gHelloWorld = NULL; // image
-SDL_Renderer* gRenderer = NULL;
+SDL_Texture* gHelloWorld = NULL; // image
 SDL_Texture* mTexture;
 int mWidth, mHeight;
 TTF_Font *gFont = NULL;
-
-bool init() {
-  bool success = true;
-
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf("SDL could not intialize. SDL_Error: %s\n", SDL_GetError());
-    success = false;
-  } else {
-    if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-    {
-      printf( "Warning: Linear texture filtering not enabled!" );
-    }
-
-    // Create window
-    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (gWindow == NULL) {
-      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-      success = false;
-    } else {
-      // create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED);
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			} else {
-        printf("hey");
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-        // init png loading
-        int imgFlags = IMG_INIT_PNG;
-        printf("Image flags: %i\n", imgFlags);
-        if (!(IMG_Init(imgFlags) & imgFlags)) {
-          printf("SDL_Image could not initialize: %s\n", IMG_GetError());
-          success = false;
-        }
-
-        if (TTF_Init() == -1) {
-          printf("SDL_ttf could not initialize: %s\n", TTF_GetError());
-          success = false;
-        }
-      }
-
-
-        //Get window surface
-        //gScreenSurface = SDL_GetWindowSurface(gWindow);
-    }
-
-  }
-  return success;
-}
 
 SDL_Surface* loadSurface(std::string path) {
   SDL_Surface* optimizedSurface = NULL;
@@ -98,14 +42,14 @@ SDL_Surface* loadSurface(std::string path) {
   return optimizedSurface;
 }
 
-SDL_Texture* loadTexture(std::string path) {
+SDL_Texture* loadTexture(std::string path, Window win) {
   // final texture
   SDL_Texture* newTexture = NULL;
   SDL_Surface* loadedSurface = IMG_Load(path.c_str());
   if (loadedSurface == NULL) {
     printf("Unable to load image %s. SDL Error: %s\n", path.c_str(), IMG_GetError());
   } else {
-    newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+    newTexture = SDL_CreateTextureFromSurface(win.renderer, loadedSurface);
     if (newTexture == NULL) {
       printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
     }
@@ -114,22 +58,22 @@ SDL_Texture* loadTexture(std::string path) {
   return newTexture;
 }
 
-bool loadMedia() {
+bool loadMedia(Window win) {
   bool success = true;
-  //gHelloWorld = loadSurface("images/51niHMPxChL.jpg");
-  // if (gHelloWorld == NULL) {
-  //   printf("Unable to load image %s! SDL Error: %s\n", "images/51niHMPxChL.jpg", SDL_GetError());
-  //   success = false;
-  // }
+  gHelloWorld = loadTexture("images/51niHMPxChL.jpg", win);
+  if (gHelloWorld == NULL) {
+    printf("Unable to load image %s! SDL Error: %s\n", "images/51niHMPxChL.jpg", SDL_GetError());
+    success = false;
+  }
 
-  gFont = TTF_OpenFont( "16_true_type_fonts/lazy.ttf", 28 );
+  gFont = TTF_OpenFont( "fonts/open-sans/OpenSans-Regular.ttf", 38 );
   SDL_Color textColor = { 0, 0, 0 };
   SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, "Hello", textColor);
   if (textSurface == NULL) {
      printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
      success = false;
   } else {
-     mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
+     mTexture = SDL_CreateTextureFromSurface(win.renderer, textSurface);
      if( mTexture == NULL ) {
        printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
        success = false;
@@ -143,19 +87,31 @@ bool loadMedia() {
   return success;
 }
 
-void close() {
+void close(Window win) {
   printf("closing\n");
-  SDL_FreeSurface(gHelloWorld);
+  win.close();
+//  SDL_FreeSurface(gHelloWorld);
   TTF_CloseFont(gFont);
-  SDL_DestroyWindow(gWindow);
-  SDL_DestroyRenderer(gRenderer);
   gFont = NULL;
   gHelloWorld = NULL;
-  gWindow = NULL;
-  gRenderer = NULL;
   TTF_Quit();
   IMG_Quit();
   SDL_Quit();
+}
+
+bool doesIntersect(int pointX, int pointY, int targetWidth, int targetHeight, int targetX, int targetY) {
+  int targetRight = targetWidth + targetX;
+  int targetBottom = targetHeight + targetY;
+  if (pointX < targetX) {
+    return false;
+  } else if (pointY < targetY) {
+    return false;
+  } else if (pointX > targetRight) {
+    return false;
+  } else if (pointY > targetBottom) {
+    return false;
+  }
+  return true;
 }
 
 void poll_events(SDL_Event e, bool* quit) {
@@ -175,16 +131,60 @@ void poll_events(SDL_Event e, bool* quit) {
           printf("keypress: %i\n", e.key.keysym.sym);
           break;
       }
+    } else if (e.type == SDL_MOUSEBUTTONDOWN) {
+      int x, y;
+      SDL_GetMouseState(&x, &y);
+      bool clickedButton = doesIntersect(x, y, mWidth, mHeight, ( SCREEN_WIDTH - mWidth ) / 2, ( SCREEN_HEIGHT - mHeight ) / 2);
+      if (clickedButton) {
+        printf("Clicked button\n");
+      }
     }
   }
 }
 
+void render(Window win) {
+  // SDL_RenderClear( gRenderer );
+
+  // //Render texture to screen
+  SDL_RenderCopy( win.renderer, mTexture, NULL, NULL );
+  SDL_RenderCopy( win.renderer, gHelloWorld, NULL, NULL );
+
+  // clear screen
+  SDL_SetRenderDrawColor( win.renderer, 0x00, 0xFF, 0xFF, 0xFF );
+  SDL_RenderClear( win.renderer );
+
+	//Render current frame
+	SDL_Rect renderQuad = { ( SCREEN_WIDTH - mWidth ) / 2, ( SCREEN_HEIGHT - mHeight ) / 2, mWidth, mHeight };
+
+	 //Set clip rendering dimensions
+  SDL_Rect* clip = NULL;
+	if( clip != NULL )
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+	SDL_RenderCopyEx( win.renderer, gHelloWorld, clip, &renderQuad, 0.0, NULL, SDL_FLIP_NONE );
+	SDL_RenderCopyEx( win.renderer, mTexture, clip, &renderQuad, 0.0, NULL, SDL_FLIP_NONE );
+
+  //Update screen
+  SDL_RenderPresent( win.renderer );
+
+  //SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+  //SDL_UpdateWindowSurface(gWindow);
+
+  //Fill the surface white
+  // SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xBB, 0xBB, 0xBB ) );
+}
+
 void run() {
-  if (!init()) {
+  Window window;
+  if (!window.init()) {
     printf("Failed to initalize\n");
   } else {
     printf("init done\n");
-    if (!loadMedia()) {
+    //FIX? window passed by value?
+    if (!loadMedia(window)) {
       printf("Failed to load media\n");
     } else {
       bool quit = false;
@@ -192,42 +192,11 @@ void run() {
 
       while (!quit) {
         poll_events(e, &quit);
-
-
-        // SDL_RenderClear( gRenderer );
-
-        // //Render texture to screen
-        SDL_RenderCopy( gRenderer, mTexture, NULL, NULL );
-
-        // clear screen
-        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-        SDL_RenderClear( gRenderer );
-
-      	//Render current frame
-      	SDL_Rect renderQuad = { ( SCREEN_WIDTH - mWidth ) / 2, ( SCREEN_HEIGHT - mHeight ) / 2, mWidth, mHeight };
-
-      	 //Set clip rendering dimensions
-        SDL_Rect* clip = NULL;
-      	if( clip != NULL )
-      	{
-      		renderQuad.w = clip->w;
-      		renderQuad.h = clip->h;
-      	}
-
-      	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, 0.0, NULL, SDL_FLIP_NONE );
-
-        //Update screen
-        SDL_RenderPresent( gRenderer );
-
-        //SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-        //SDL_UpdateWindowSurface(gWindow);
+        render(window);
       }
     }
   }
-  //Fill the surface white
-  // SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xBB, 0xBB, 0xBB ) );
-
-  close();
+  close(window);
 }
 
 int main(int argc, char* args[]) {
---
+++ sdlgame/src/window.h
@@ -0,0 +1,67 @@
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+class Window {
+public:
+  SDL_Window* window;
+  SDL_Renderer* renderer;
+  int someInt;
+  Window() : window(NULL), renderer(NULL), someInt(0) {}
+
+  void set(int integer) {
+    someInt = integer;
+  }
+
+  void close() {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    window = NULL;
+    renderer = NULL;
+  }
+
+  bool init() {
+    bool success = true;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      printf("SDL could not intialize. SDL_Error: %s\n", SDL_GetError());
+      success = false;
+    } else {
+      if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+      {
+        printf( "Warning: Linear texture filtering not enabled!" );
+      }
+
+      // Create window
+      window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+      if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+      } else {
+        // create vsynced renderer for window
+  			renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+  			if( renderer == NULL )
+  			{
+  				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+  				success = false;
+  			} else {
+          printf("hey");
+  				//Initialize renderer color
+  				SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+          // init png loading
+          int imgFlags = IMG_INIT_PNG;
+          printf("Image flags: %i\n", imgFlags);
+          if (!(IMG_Init(imgFlags) & imgFlags)) {
+            printf("SDL_Image could not initialize: %s\n", IMG_GetError());
+            success = false;
+          }
+
+          if (TTF_Init() == -1) {
+            printf("SDL_ttf could not initialize: %s\n", TTF_GetError());
+            success = false;
+          }
+        }
+      }
+    }
+    return success;
+  }
+};
